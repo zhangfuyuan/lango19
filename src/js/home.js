06c2e2ed024f3a636762732942aa0666066e93db
zhangfuyuan <fuyuan.zhang@lango-tech.cn>
2019-09-06 12:24:41 +0800

    兼容火狐、IE8+、360、QQ、UC等浏览器

--- src/js/home.js
+++ src/js/home.js
@@ -16,6 +16,5 @@ $.extend(true, window.Page || (window.Page = {}), {
 /********************************************* 以上声明，以下调用 *********************************************/
 
 if (Page.isIE() && Page.isNotSupportIE()) {
-  alert('系统检测到您正在使用IE' + Page.supportIEVersion + '以下内核的浏览器（实际IE' + Page.ua.match(/msie ([\d.]+)/)[1] +
-    '），不能实现完美体验，请更换或升级浏览器访问！');
+  alert('系统检测到您正在使用IE' + Page.supportIEVersion + '以下内核的浏览器，不能实现完美体验，请更换或升级浏览器访问！');
 }
